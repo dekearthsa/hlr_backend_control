e76eb1d2a58debe9260c4945f52785655bdea53b
pcsishun <dekearthsa@hotmail.com>
2025-10-21 20:40:26 +0700

    add service

--- index.js
+++ index.js
@@ -420,7 +420,7 @@ app.post('/loop/data/iaq', async (request, reply) => {
 });
 
 
-app.listen({ port: 3011 }, (err, address) => {
+app.listen({ port: 3011, host: '0.0.0.0' }, (err, address) => {
     if (err) {
         app.log.error(err)
         process.exit(1)
